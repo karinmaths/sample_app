ca35da45db9040126f831d48ce019f1df7802fe8
karinmaths <karin.maths@gmail.com>
2012-12-08 19:02:34 +0700

    Route fundamental pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,25 +2,29 @@ require 'spec_helper'
 
 describe "Static pages" do
 	
+	subject { page }
+
 	describe "Home page" do
-		it "should have the h1 'Sample App'" do
-			visit '/static_pages/home'
-			page.should have_selector('h1', :text => 'Sample App')
-		end
+		before { visit root_path }
+
+		it { should have_selector('h1', text: 'Sample App')}
 	end
 
 	describe "Help page" do
-		it "should have the h1 'Help'" do
-			visit '/static_pages/help'
-			page.should have_selector('h1', :text => 'Help')
-		end
+		before { visit help_path }
+
+		it { should have_selector('h1', text: 'Help')}
 	end
 
 	describe "About page" do
-		it "should have the h1 'About Us'" do
-			visit '/static_pages/about'
-			page.should have_selector('h1', :text => 'About Us')
-		end
+		before { visit about_path }
+
+		it { should have_selector('h1', text: 'About Us')}
 	end
 
+	describe "Contact page" do
+		before { visit contact_path }
+
+		it { should have_selector('h1', text: 'Contact')}
+	end
 end
